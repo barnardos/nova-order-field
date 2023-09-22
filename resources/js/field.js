a7cbe89d6a611025b7b349cde079d62a57b22028
David Ogilo <david.ogilo@dayesoft.co.uk>
2023-09-22 09:45:24 +0100

    [] Updated field.js

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,3 +1,3 @@
 Nova.booting((Vue, router) => {
-    Vue.component('index-order-field', require('./components/IndexField'));
+    Nova.inertia('index-order-field', require('./components/IndexField'));
 })
